f628d4dbf7ee4f3dad543762ac5ec7bf7b86dfa5
ryandoherty1 <37849271+ryandoherty1@users.noreply.github.com>
2020-01-17 15:42:34 -0800

    Add files via upload

---
+++ strangerthings.rb
@@ -0,0 +1,21 @@
+use_bpm 160
+use_synth :piano
+play :A3
+play :A2
+live_loop :melody do
+  play :C3
+  sleep 0.5
+  play :E4
+  sleep 0.5
+  play :G4
+  sleep 0.5
+  play :B4
+  sleep 0.5
+  play :C4
+  sleep 0.5
+  play :B4
+  sleep 0.5
+  play :G4
+  sleep 0.5
+  play :C4
+end
